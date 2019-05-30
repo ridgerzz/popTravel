1f865e3e10258405d4bf660b5f1e8a1361939481
Ridge Lanuza <ridgehh@yahoo.com>
2019-05-29 19:19:34 -0700

    Initial Commit

---
+++ popTravel/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  popTravel
+//
+//  Created by Ridge Lanuza on 5/29/19.
+//  Copyright © 2019 Ridge Lanuza. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ popTravel/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  popTravel
+//
+//  Created by Ridge Lanuza on 5/29/19.
+//  Copyright © 2019 Ridge Lanuza. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
