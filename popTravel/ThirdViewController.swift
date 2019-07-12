7cc370429457b0f932ac6ffe04dff6528b0edd35
Ridge Lanuza <ridgehh@yahoo.com>
2019-07-12 15:59:34 -0700

    FS calendar on third view controller w/ gesture controls

--- popTravel/ThirdViewController.swift
+++ popTravel/ThirdViewController.swift
@@ -9,27 +9,131 @@
 import UIKit
 import FSCalendar
 
-class ThirdViewController: UIViewController {
+class ThirdViewController: UIViewController, FSCalendarDataSource, FSCalendarDelegate, UITableViewDataSource, UITableViewDelegate, UIGestureRecognizerDelegate {
     
-    fileprivate weak var calendar: FSCalendar!
+    //class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, FSCalendarDataSource, FSCalendarDelegate {
+    
+//    @IBOutlet weak var tableView: UITableView!
+//    @IBOutlet weak var calendar: FSCalendar!
+//    @IBOutlet weak var calendarHeightConstraint: NSLayoutConstraint!
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var calendar: FSCalendar!
+    @IBOutlet weak var calendarHeightConstraint: NSLayoutConstraint!
+    
+    fileprivate lazy var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy/MM/dd"
+        return formatter
+    }()
+    
+    fileprivate lazy var scopeGesture: UIPanGestureRecognizer = {
+        [unowned self] in
+        let panGesture = UIPanGestureRecognizer(target: self.calendar, action: #selector(self.calendar.handleScopeGesture(_:)))
+        panGesture.delegate = self
+        panGesture.minimumNumberOfTouches = 1
+        panGesture.maximumNumberOfTouches = 2
+        return panGesture
+        }()
+    
+    var currentMonthSymbol: String {
+        get {
+            let currentPageDate = calendar.currentPage
+            let month = Calendar.current.component(.month, from: currentPageDate)
+            let monthString = DateFormatter().monthSymbols[month-1]
+            return monthString
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let calendar = FSCalendar(frame: CGRect(x: 0, y: 0, width: 320, height: 300))
-        //        calendar.dataSource = self
-        //        calendar.delegate = self
-        view.addSubview(calendar)
-        self.calendar = calendar
+        self.calendar.select(Date())
+        
+        self.view.addGestureRecognizer(self.scopeGesture)
+        self.tableView.panGestureRecognizer.require(toFail: self.scopeGesture)
+        
+        // For UITest
+        self.calendar.accessibilityIdentifier = "calendar"
+        
+        currentMonthYear()
+        
+//        self.calendar.calendarHeaderView.isHidden = true
+//        self.calendar.headerHeight = 4 // this makes some extra spacing, but you can try 0 or 1
+        
     }
     
+    // MARK:- UIGestureRecognizerDelegate
     
-}
-
-extension ThirdViewController: FSCalendarDataSource, FSCalendarDelegate {
+    func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        let shouldBegin = self.tableView.contentOffset.y <= -self.tableView.contentInset.top
+        if shouldBegin {
+            let velocity = self.scopeGesture.velocity(in: self.view)
+            switch self.calendar.scope {
+            case .month:
+                return velocity.y < 0
+            case .week:
+                return velocity.y > 0
+            @unknown default:
+                fatalError()
+            }
+        }
+        return shouldBegin
+    }
     
+    func calendar(_ calendar: FSCalendar, boundingRectWillChange bounds: CGRect, animated: Bool) {
+        self.calendarHeightConstraint.constant = bounds.height
+        self.view.layoutIfNeeded()
+    }
+    func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
+        print("did select date \(self.dateFormatter.string(from: date))")
+        let selectedDates = calendar.selectedDates.map({self.dateFormatter.string(from: $0)})
+        print("selected dates is \(selectedDates)")
+        if monthPosition == .next || monthPosition == .previous {
+            calendar.setCurrentPage(date, animated: true)
+        }
+    }
+    
+    // gets month
+    func calendarCurrentPageDidChange(_ calendar: FSCalendar) {
+        currentMonthYear()
+    }
+    
+    func currentMonthYear () {
+        let currentPageDate = calendar.currentPage
+        let year = Calendar.current.component(.year, from: currentPageDate)
+        self.title = "\(currentMonthSymbol) \(year)"
+        
+        
+    }
+    
+    // MARK:- UITableViewDataSource
+    
+    //    func numberOfSections(in tableView: UITableView) -> Int {
+    //        return 2
+    //    }
+    //
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
+        
+        return cell
+        
+    }
+    
+    // MARK:- UITableViewDelegate
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        if indexPath.section == 0 {
+            if self.calendar.scope == .month {
+                self.calendar.setScope(.week, animated: true)
+            }
+        }
+    }
 }
-
-
-
 
