4de79ec5c3f590949131428c6f79ba893812d2a1
Ridge Lanuza <ridgehh@yahoo.com>
2019-07-25 17:50:31 -0700

    Profile/Mainpage
    Worked on Profile/Account Page. Added basic side menu that pushes to a seperate page

---
+++ AccountViewController.swift
@@ -0,0 +1,16 @@
+//
+//  AccountViewController.swift
+//  
+//
+//  Created by Ridge Lanuza on 7/25/19.
+//
+
+import UIKit
+
+class AccountViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
