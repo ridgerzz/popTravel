a3bcf70302345c9127bda56b23b2b84568773eb5
Ridge Lanuza <ridgehh@yahoo.com>
2019-08-15 12:46:09 -0700

    Networking (part 9)
    added struct for api and edited app delegates

---
+++ popTravel/Screen/List/Models.swift
@@ -0,0 +1,36 @@
+//
+//  Models.swift
+//  popTravel
+//
+//  Created by Ridge Lanuza on 8/14/19.
+//  Copyright © 2019 Ridge Lanuza. All rights reserved.
+//
+
+import Foundation
+
+struct Root: Codable {
+    let businesses: [Business]
+}
+
+struct Business: Codable {
+    let id: String
+    let name: String
+    let imageUrl: URL
+    let distance: Double
+}
+
+struct RestaurantListViewModel {
+    let name: String
+    let imageUrl: URL
+    let distance: String
+    let id: String
+}
+
+extension RestaurantListViewModel {
+    init(business: Business) {
+        self.name = business.name
+        self.id = business.id
+        self.imageUrl = business.imageUrl
+        self.distance = "\(business.distance / 1609.344)"
+    }
+}
